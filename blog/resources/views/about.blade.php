eaa5150f07ec995490710667e70ee0ce16484887
arundhati2409 <arundhati.amexs@gmail.com>
2018-11-10 11:37:33 +0530

    blade templates of existing html

---
+++ blog/resources/views/about.blade.php
@@ -0,0 +1,258 @@
+@extends('layout')
+
+@section('body')
+    <!--// banner-text -->
+    <!-- /breadcrumb -->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.html">Home</a>
+        </li>
+        <li class="breadcrumb-item active">About</li>
+    </ol>
+    <!-- //breadcrumb -->
+    <!-- banner-bottom-w3ls -->
+    <section class="banner-bottom-w3ls pb-lg-5 pb-md-5 pb-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 about-img">
+                </div>
+                <div class="col-lg-6 about-right">
+                    <h4>Who We Are</h4>
+                    <h3>We give you complete control of your shipments.</h3>
+                    <p class="my-4">Lorem ipsum dolor sit amet Neque porro quisquam est qui dolorem Lorem int ipsum dolor sit amet when an unknown printer took a galley of type.Vivamus id tempor felis. Cras sagittis mi sit amet malesuada mollis. Mauris porroinit consectetur cursus tortor vel interdum.</p>
+
+                    <ul class="author d-flex">
+                        <li><img class="img-fluid" src="images/author.jpg" alt=""></li>
+                        <li><span>Admin Name</span>Comany Namer</li>
+                    </ul>
+                    <div class="log-in mt-md-5 mt-3">
+                        <a class="btn text-uppercase" href="single.html">
+                                    Read More</a>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //banner-bottom-w3ls -->
+    <!-- ab -->
+    <section class="banner-bottom-w3ls py-lg-5 py-md-5 py-3">
+
+        <div class="container">
+            <div class="inner-sec-w3layouts py-lg-5 py-3">
+                <div class="intro text-left">
+                    <h3 class="main mb-md-5 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore et dolore.</h3>
+                    <p>Adipisicing elit sedc eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+                <div class="row mt-lg-5 mt-md-4 mt-4">
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-anchor mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Ocean Freight</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <i class="fas fa-fighter-jet mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Air Freight</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-truck mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Land Transport</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- //ab -->
+    <!-- /bottom-last -->
+    <section class="bottom-last py-5 text-center">
+        <div class="bottom-bg">
+            <h3>Imagination… What we can easily see is only a small percentage of what is possible.</h3>
+
+        </div>
+    </section>
+    <!-- //bottom-last -->
+    <!--/Blog-Posts-->
+    <section class="banner-bottom-w3ls bg-light py-md-5 py-4">
+        <div class="container">
+            <div class="inner-sec-w3layouts py-md-5 py-4">
+                <h3 class="tittle text-center mb-md-5 mb-4">
+                    Blog Posts</h3>
+                <!--/services-grids-->
+                <div class="row blog-sec">
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g2.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">June 18, 2018</span>
+                                    <h5 class="card-title text-uppercase mt-2"><a href="single.html">Vivamus id tempor felis. Cras sagittis mi sit amet </a></h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g3.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">June 20, 2018</span>
+                                    <h5 class="card-title text-uppercase mt-2"><a href="single.html">Vivamus id tempor felis. Cras sagittis mi sit amet </a></h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g4.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">June 25, 2018</span>
+                                    <h5 class="card-title text-uppercase mt-2"><a href="single.html">Vivamus id tempor felis. Cras sagittis mi sit amet </a></h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//Blog-Posts-->
+    <!--/testimonials-->
+    <section class="clients py-md-5 py-3">
+        <div class="container">
+            <div class="inner-sec-w3layouts py-md-5 py-3">
+                <h3 class="tittle text-center mb-md-5 mb-4">Some word from our Clients</h3>
+                <div class="owl-carousel owl-theme">
+                    <div class="item">
+                        <div class="feedback-info text-left">
+                            <div class="feedback-top">
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore.</p>
+                            </div>
+                            <div class="feedback-grids">
+                                <div class="feedback-img">
+                                    <img src="images/t1.jpg" class="img-fluid rounded-circle" alt="" />
+                                </div>
+                                <div class="feedback-img-info">
+                                    <h5>Mary Jane</h5>
+                                    <p>United States
+                                        <span>(Company)</span>
+                                    </p>
+                                </div>
+                                <div class="clearfix"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="feedback-info text-left">
+
+                            <div class="feedback-top">
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore.</p>
+                            </div>
+                            <div class="feedback-grids">
+                                <div class="feedback-img">
+                                    <img src="images/t3.jpg" class="img-fluid rounded-circle" alt="" />
+                                </div>
+                                <div class="feedback-img-info">
+                                    <h5>Steven Wilson</h5>
+                                    <p>United States
+                                        <span>(Company)</span>
+                                    </p>
+                                </div>
+                                <div class="clearfix"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="feedback-info text-left">
+                            <div class="feedback-top">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore.</p>
+                            </div>
+                            <div class="feedback-grids">
+                                <div class="feedback-img">
+                                    <img src="images/t2.jpg" class="img-fluid rounded-circle" alt="" />
+                                </div>
+                                <div class="feedback-img-info">
+                                    <h5>Peter guptill</h5>
+                                    <p>Vestibulum <span>(Company)</span></p>
+                                </div>
+                                <div class="clearfix"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="feedback-info text-left">
+
+                            <div class="feedback-top">
+
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore.</p>
+                            </div>
+                            <div class="feedback-grids">
+                                <div class="feedback-img">
+                                    <img src="images/t3.jpg" class="img-fluid rounded-circle" alt="" />
+                                </div>
+                                <div class="feedback-img-info">
+                                    <h5>Steven Wilson</h5>
+                                    <p>United States
+                                        <span>(Company)</span>
+                                    </p>
+                                </div>
+                                <div class="clearfix"> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="feedback-info text-left">
+                            <div class="feedback-top">
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore.</p>
+                            </div>
+                            <div class="feedback-grids">
+                                <div class="feedback-img">
+                                    <img src="images/t2.jpg" class="img-fluid rounded-circle" alt="" />
+                                </div>
+                                <div class="feedback-img-info">
+                                    <h5>Peter guptill</h5>
+                                    <p>Vestibulum <span>(Company)</span></p>
+                                </div>
+                                <div class="clearfix"> </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//testimonials-->
+    <!--/newsletter-->
+ @endsection
---
+++ blog/resources/views/services.blade.php
@@ -0,0 +1,195 @@
+@extends('layout')
+
+
+
+@section('body')
+    <!--//banner-->
+    <!-- /breadcrumb -->
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">
+            <a href="index.html">Home</a>
+        </li>
+        <li class="breadcrumb-item active">Services</li>
+    </ol>
+    <!-- //breadcrumb -->
+    <!-- / -->
+    <section class="banner-bottom-w3ls py-lg-5 py-md-5 py-3">
+
+        <div class="container">
+            <div class="inner-sec-w3layouts py-lg-5 py-3">
+                <div class="intro text-left">
+                    <h3 class="main mb-md-5 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit sedc dnmo eiusmod tempor incididunt ut labore et dolore.</h3>
+                    <p>Adipisicing elit sedc eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+                <div class="row mt-lg-5 mt-md-4 mt-4">
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-anchor mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Ocean Freight</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+
+                                <i class="fas fa-fighter-jet mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Air Freight</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-truck mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Land Transport</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fab fa-docker mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Supply</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="far fa-clock mb-2"></i>
+
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Fast & On Time</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-lg-4 about-in text-left">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="far fa-money-bill-alt mb-2"></i>
+                                <h6 class="my-3">Consectetur elit</h6>
+                                <h5 class="card-title">Weather Insuarance</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur elit,Adipisicing elit tempor.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!--services-->
+        <!-- /services -->
+    <section class="banner-bottom-w3ls bg-dark py-lg-5 py-md-5 py-3">
+        <div class="container">
+            <div class="inner-sec-w3layouts py-lg-5 py-3">
+                <h3 class="tittle text-center text-white mb-md-5 mb-4">We Offered Services</h3>
+                <div class="row middle-grids">
+                    <div class="col-lg-4 about-in middle-grid-info text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-archive mb-2"></i>
+                                <h5 class="card-title my-3">Storage</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in middle-grid-info text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-signal mb-2"></i>
+                                <h5 class="card-title my-3">Trucking Services</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in middle-grid-info text-center">
+                        <div class="card">
+                            <div class="card-body">
+                                <i class="fas fa-users mb-2"></i>
+                                <h5 class="card-title my-3">Expert Staff</h5>
+                                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--services-->
+    <!--/featured-->
+    <section class="banner-bottom-w3ls py-md-5 py-4">
+        <div class="container">
+            <div class="inner-sec-w3layouts py-md-5 py-4">
+                <h3 class="tittle text-center mb-md-5 mb-4">
+                    Featured Services</h3>
+                <!--/services-grids-->
+                <div class="row blog-sec">
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g2.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">Consectetur</span>
+                                    <h5 class="card-title text-uppercase mt-2">Vivamus id tempor felis. Cras sagittis mi sit amet </h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g3.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">Consectetur</span>
+                                    <h5 class="card-title text-uppercase mt-2">Vivamus id tempor felis. Cras sagittis mi sit amet </h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 about-in blog-grid-info text-left">
+                        <div class="card img">
+                            <div class="card-body img">
+                                <img src="images/g4.jpg" alt="" class="img-fluid">
+                                <div class="blog-des">
+                                    <span class="entry-date">Consectetur</span>
+                                    <h5 class="card-title text-uppercase mt-2">Vivamus id tempor felis. Cras sagittis mi sit amet </h5>
+                                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//featured-->
+
+    <!--/newsletter-->
+@endsection
